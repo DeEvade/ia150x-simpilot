4b82a27596b2a04cc5935a22162c08addf56ab32
Aron Tiselius <aron.tiselius@gmail.com>
2025-03-20 14:24:17 +0100

    pseudo tts

---
+++ backend/src/tts.ts
@@ -0,0 +1,34 @@
+const voiceModels = {'p225', 'p226', 'p227'};
+const ip = 0.0.0.0;
+const port = 1337;
+let availableModels = Array.from(voiceModels);
+
+        /*
+const textToSpeech = async(command : string, plane : id) => {
+    try{
+       let voiceModel = id.voice; 
+       doTTSthingy(command, voiceModel)
+    }
+    catch(error){
+
+    }
+}
+
+const newPlaneArrivedInsideOfTheComputerizedSimulator = (plane : id) => {
+    const length = availableModels.length
+    if(length < 1 ){
+        idk
+    }
+    const i = Math.floor(Math.random() * length);
+    const voiceModel = availableModels[i];
+    availableModels.splice(i,1); //ta bort modellen från listan
+    id.voice = voiceModel;
+}
+
+const planeDisappearedFromTheInsideOfTheComputerizedSimulator = (plane : id) => {
+    availableModels.push(id.voice)
+    id = null;
+}
+            */
+
+
